7f0eef48a3281737440766b85cc6d26d695cfbd5
unknown <Vwede Oteghele>
2021-08-25 04:20:48 -0700

    model creation for quote, vehicle

---
+++ src/models/vehicle.js
@@ -0,0 +1,49 @@
+const { model, Schema } = require('mongoose');
+
+const VehicleSchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+        default: 0,
+    },
+    chassis: {
+        type: Number,
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    clientId: {
+        type: Number,
+        required: true,
+      },
+    // Model Required fields
+    isActive: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    timeStamp: {
+        type: Number,
+        required: true,
+        default: () => Date.now(),
+    },
+    createdOn: {
+        type: Date,
+        required: true,
+        default: () => new Date(),
+    },
+    updatedOn: {
+        type: Date,
+        required: true,
+        default: () => new Date(),
+    },
+});
+
+model('Vehicle', VehicleSchema);
