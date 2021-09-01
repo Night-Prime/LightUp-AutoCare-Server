427bc64a4fce22c63e9329b8cd7bc64d9d73491c
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2021-09-01 14:27:18 +0100

    Corrected validation error

--- src/validators/staff.js
+++ src/validators/staff.js
@@ -4,6 +4,6 @@ module.exports = Joi.object({
     id: Joi.string().min(1).max(Number.MAX_SAFE_INTEGER),
     name: Joi.string().required().min(3).max(30),
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    role: Joi.string().valid('admin', 'approver', 'staff').required(),
+    role: Joi.string().valid('admin', 'approver', 'clerk').required(),
     password: Joi.string(),
 });
