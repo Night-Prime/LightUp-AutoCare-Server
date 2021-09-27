55165d13752e372f455fe4cd57222dec3806fbb0
unknown <Vwede Oteghele>
2021-09-27 08:50:04 +0100

    new commit

--- src/services/quote/quote.js
+++ src/services/quote/quote.js
@@ -74,13 +74,6 @@ class QuoteService extends RootService {
     }
 
     async readRecordsByFilter(request, next) {
-        // const { query } = request;
-        // let result;
-
-        // if (result.failed) {
-        //     throw new Error(result.error);
-        // } else {
-        //     return this.processMultipleReadResults(result);
         try {
             const { query } = request;
 
