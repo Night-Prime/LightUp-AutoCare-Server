07053d7fb3d11068d27fe3e8c7ca36ae3e287655
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2021-10-12 14:32:41 +0100

    Updated smtp for create password

--- src/events/createPassword.js
+++ src/events/createPassword.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+require('dotenv').config();
 const { sendMail, generateToken } = require('../utilities/packages');
 const BASE_URL = 'https://light-up-auto-care.herokuapp.com';
 
@@ -13,18 +14,22 @@ createPasswordEmitter.on('createPassword', async function (request, user) {
         name: user.name,
     });
     const emailContent = {
-        from: process.env.email, // sender address
+        from: process.env.outlookEmail, // sender address
         to: user.email,
         subject: `Update profile by creating password`,
         text: `Hi ${user.name}. \n Update your password for your account by clicking this link below. This link would be valid within the next 24hrs for updating your password.\n ${BASE_URL}/createPassword?password_token=${token}`,
         html: '<b>LightUp AutoCare</b>',
     };
     const transportPayload = {
-        host: 'smtp.mailtrap.io',
-        port: 2525,
+        host: 'smtp-mail.outlook.com', // hostname
+        secureConnection: false, // TLS requires secureConnection to be false
+        port: 587, // port for secure SMTP
+        tls: {
+            ciphers: 'SSLv3',
+        },
         auth: {
-            user: process.env.testEmail,
-            pass: process.env.testEmailPassword,
+            user: process.env.outlookEmail,
+            pass: process.env.outlookEmailPassword,
         },
     };
     await sendMail(transportPayload, emailContent).catch((error) => {
