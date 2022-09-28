c8964345859f7e9f59e69c84dc9766b9fe371b6b
Daniel Abati <danielabatibabatunde1@gmail.com>
2022-09-28 11:23:25 +0100

    mongo-fix

--- src/models/staff.js
+++ src/models/staff.js
@@ -7,10 +7,6 @@ const StaffSchema = new Schema({
         unique: true,
         default: 0,
     },
-    _id: {
-        type: Number,
-        required: true,
-    },
     name: {
         type: String,
         required: true,
